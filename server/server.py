9085cd2726efb1b948dd176a5e9b5a69b15777f5
EneVyctis <roux.basi@gmail.com>
2024-04-27 21:58:52 +0200

    Update server, ouvre un port pour la detection wifi fi (8080) et un pour bluetooth (8000)

--- server/server.py
+++ server/server.py
@@ -4,10 +4,11 @@ import csv
 import time
 
 ADRESSE = ''  # L'adresse IP sur laquelle le serveur écoute (ici, toutes les interfaces disponibles)
-PORT = 8080   # Le port sur lequel le serveur écoute
-listOfMacs = {}
+PORT = [8080,8000]   # Le port sur lequel le serveur écoute
+listOfMacsWifi = {}
+listOfMacsBluetooth = {}
 # Fonction gérant chaque client
-def gerer_client(client, adresse):
+def gerer_client_wifi(client, adresse):
     print(f"Connexion de {adresse}")
     try:
         while True:
@@ -22,17 +23,48 @@ def gerer_client(client, adresse):
                         arreter_serveur()
                         return
                 elif stringDonnees.strip().lower()[:-1] == "/count":
-                        count = len(listOfMacs)
+                        count = len(listOfMacsWifi)
                         print(f"Il y a actuellement {count} personnes dans la zone")
                 elif stringDonnees.strip().lower()[:-1] == "/list": 
-                        for mac in listOfMacs:
+                        for mac in listOfMacsWifi:
                                 print(mac)
                 else:
-                    print(len(stringDonnees))
                     for k in range(len(stringDonnees)//17):
                         i = k*17
-                        print(stringDonnees[i:i+17])
-                        listOfMacs[stringDonnees[i:i+17]] = time.time()
+                        listOfMacsWifi[stringDonnees[i:i+17]] = time.time()
+    except Exception as e:
+        print(f"Erreur lors de la communication avec {adresse}: {e}")
+    
+    # Ferme la connexion avec le client
+    try:
+        client.close()
+    except Exception as e:
+        print(f"Erreur lors de la fermeture de la connexion avec {adresse}: {e}")
+
+def gerer_client_bluetooth(client, adresse):
+    print(f"Connexion de {adresse}")
+    try:
+        while True:
+            donnees = client.recv(1024)  # Reçoit les données du client, jusqu'à 1024 octets à la fois
+            if not donnees:
+                print(f"Client {adresse} déconnecté.")
+                return
+            else:
+                stringDonnees = donnees.decode('utf-8')  # Décode les données en UTF-8
+                print(f"Reception de {adresse}: {stringDonnees}")
+                if stringDonnees.strip().lower()[:-1] == "/quit":
+                        arreter_serveur()
+                        return
+                elif stringDonnees.strip().lower()[:-1] == "/count":
+                        count = len(listOfMacsBluetooth)
+                        print(f"Il y a actuellement {count} personnes dans la zone")
+                elif stringDonnees.strip().lower()[:-1] == "/list": 
+                        for mac in listOfMacsBluetooth:
+                                print(mac)
+                else:
+                    for k in range(len(stringDonnees)//17):
+                        i = k*17
+                        listOfMacsBluetooth[stringDonnees[i:i+17]] = time.time()
     except Exception as e:
         print(f"Erreur lors de la communication avec {adresse}: {e}")
     
@@ -48,11 +80,14 @@ def actualiser_macs():
         time.sleep(20)
         t = time.time()
         # Filtrer les clients inactifs
-        macs_inactifs = [mac for mac in listOfMacs if listOfMacs[mac] < t - 40]
-        for mac in macs_inactifs:
+        macs_inactifs_wi = [mac for mac in listOfMacsWifi if listOfMacsWifi[mac] < t - 40]
+        macs_inactifs_blue = [mac for mac in listOfMacsBluetooth if listOfMacsBluetooth[mac] < t - 40]
+        for mac in macs_inactifs_wi:
             print(f"Supprime {mac} de la liste des MACs")
-            listOfMacs.pop(mac)
-
+            listOfMacsWifi.pop(mac)
+        for mac in macs_inactifs_blue:
+            print(f'Supprime {mac} de la liste des MACs')
+            listOfMacsBluetooth.pop(mac)
 # Fonction pour arrêter le serveur
 def arreter_serveur():
     print("Arrêt du serveur en cours...")
@@ -62,35 +97,55 @@ def arreter_serveur():
         if hasattr(client, 'shutdown'):
             client.shutdown(socket.SHUT_RDWR)
             client.close()
-    serveur.close()
-    print("Serveur arrêté.")
+    serveurBlutooth.close()
+    serveurWifi.close()
+
+def ManageWifi():
+    while True:
+        try:
+            # Accepte la connexion entrante et récupère le socket client et l'adresse du client
+            client, adresseClient = serveurWifi.accept()
+            # Crée un thread pour gérer ce client
+            thread_client = threading.Thread(target=gerer_client_wifi, args=(client, adresseClient))
+            # Démarre le thread
+            thread_client.start()
+        except Exception as e:
+            print(f"Erreur lors de la gestion d'une connexion entrante: {e}")
+
+def ManageBluetooth():
+    while True:
+        try:
+            # Accepte la connexion entrante et récupère le socket client et l'adresse du client
+            client, adresseClient = serveurBlutooth.accept()
+            # Crée un thread pour gérer ce client
+            thread_client = threading.Thread(target=gerer_client_bluetooth, args=(client, adresseClient))
+            # Démarre le thread
+            thread_client.start()
+        except Exception as e:
+            print(f"Erreur lors de la gestion d'une connexion entrante: {e}")
 
 # Crée un objet socket pour le serveur
-serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+serveurWifi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serveurBlutooth = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 try:
     # Associe le serveur à l'adresse et au port spécifiés
-    serveur.bind((ADRESSE, PORT))
+    serveurWifi.bind((ADRESSE, PORT[0]))
+    serveurBlutooth.bind((ADRESSE, PORT[1]))
     # Commence à écouter les connexions entrantes, en limitant à 5 clients
-    serveur.listen(5)
-    print("Serveur en attente de connexions...")
+    serveurWifi.listen(5)
+    serveurBlutooth.listen(5)
+    print("Serveurs en attente de connexions...")
 except Exception as e:
     print(f"Erreur lors du démarrage du serveur: {e}")
-    serveur.close()
+    serveurWifi.close()
+    serveurBlutooth.close()
     exit()
 # Boucle principale pour accepter les connexions entrantes
 thread_manage_list = threading.Thread(target = actualiser_macs)
 thread_manage_list.start()
-while True:
-    try:
-        # Accepte la connexion entrante et récupère le socket client et l'adresse du client
-        client, adresseClient = serveur.accept()
-        # Crée un thread pour gérer ce client
-        thread_client = threading.Thread(target=gerer_client, args=(client, adresseClient))
-        # Démarre le thread
-        thread_client.start()
-    except Exception as e:
-        print(f"Erreur lors de la gestion d'une connexion entrante: {e}")
 
-# Ferme le socket du serveur (ce code ne sera jamais atteint dans ce contexte)
-serveur.close() 
+thread_manage_wifi = threading.Thread(target = ManageWifi)
+thread_manage_wifi.start()
+
+thread_manage_bluetooth = threading.Thread(target = ManageBluetooth)
+thread_manage_bluetooth.start()
